cab5009df30f3fef6707a47a404e6cf6321e8c9c
Pat <pat@Pat-MBP.gateway>
2016-08-27 12:03:20 +1000

    removed uploadb

--- thedress/js/script.js
+++ thedress/js/script.js
@@ -17,6 +17,8 @@ $(function(){
     canvas = document.getElementById('panel');
     ctx = canvas.getContext('2d');
 
+
+
     $('#panel').mousemove(function(e) { // mouse move handler
         var canvasOffset = $(canvas).offset();
         var canvasX = Math.floor(e.pageX - canvasOffset.left);
@@ -47,7 +49,63 @@ $(function(){
         $('#rgbaVal').val(pixel[0]+','+pixel[1]+','+pixel[2]+','+pixel[3]);
         var dColor = pixel[2] + 256 * pixel[1] + 65536 * pixel[0];
         $('#hexVal').val( '#' + dColor.toString(16) );
-    }); 
+    });
+
+
+
+
+
+    // Set up touch events for mobile, etc
+    canvas.addEventListener("touchstart", function (e) {
+            mousePos = getTouchPos(canvas, e);
+      var touch = e.touches[0];
+      var mouseEvent = new MouseEvent("mousedown", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+      });
+      canvas.dispatchEvent(mouseEvent);
+    }, false);
+    canvas.addEventListener("touchend", function (e) {
+      var mouseEvent = new MouseEvent("mouseup", {});
+      canvas.dispatchEvent(mouseEvent);
+    }, false);
+    canvas.addEventListener("touchmove", function (e) {
+      var touch = e.touches[0];
+      var mouseEvent = new MouseEvent("mousemove", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+      });
+      canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+    // Get the position of a touch relative to the canvas
+    function getTouchPos(canvasDom, touchEvent) {
+      var rect = canvasDom.getBoundingClientRect();
+      return {
+        x: touchEvent.touches[0].clientX - rect.left,
+        y: touchEvent.touches[0].clientY - rect.top
+      };
+    }
+
+    // Prevent scrolling when touching the canvas
+    document.body.addEventListener("touchstart", function (e) {
+      if (e.target == canvas) {
+        e.preventDefault();
+      }
+    }, false);
+    document.body.addEventListener("touchend", function (e) {
+      if (e.target == canvas) {
+        e.preventDefault();
+      }
+    }, false);
+    document.body.addEventListener("touchmove", function (e) {
+      if (e.target == canvas) {
+        e.preventDefault();
+      }
+    }, false);
+
+
+
 
     $('#swImage').click(function(e) { // switching images
         iActiveImage++;
